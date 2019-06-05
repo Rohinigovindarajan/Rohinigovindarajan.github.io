ea0f3d7c5d74ac9bdd6727b349f0a09dccff23b1
Rohinigovindarajan <51396427+Rohinigovindarajan@users.noreply.github.com>
2019-06-05 12:24:52 +0530

    uploading

---
+++ advanced.js
@@ -0,0 +1,20 @@
+function movecommand(direction){
+	var whathappens;
+	switch(direction){
+		case "forward":
+		whathappens = "hi";
+
+		case "backward":
+		whathappens = "hello";
+
+		case "right":
+		whathappens = "fine";
+
+		case "left":
+		whathappens = "gud";
+
+		default:
+		whathappens ="sry";
+	}
+return whathappens;
+}
